002e9934ece20053ada306592c61730be30c2044
Jonatan Snyders <jonatansnyders@gmail.com>
2025-04-30 17:43:38 +0200

    added simple main loop

---
+++ playground/main.py
@@ -0,0 +1,18 @@
+import asyncio
+from mcp.client.sse import sse_client
+from mcp.client.session import ClientSession
+import mcp.types as types
+
+
+async def main():
+    print("Starting MCP client...")
+    async with sse_client("http://127.0.0.1:8050/sse") as (read, write):
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+            tools_result: types.ListToolsResult = await session.list_tools()
+            tool_names = [tool.name for tool in tools_result.tools]
+            print(f"tool_names: {tool_names}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
